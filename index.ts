40607871955fed37d0db4f3cde49052e1b6cff31
natikos <natalia.kostelna20@gmail.com>
2025-08-25 23:28:13 +0300

    Add VSCode settings, update TypeScript configuration, and fix script name in package.json

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
-import 'reflect-metadata'
+import 'reflect-metadata';
 
-import { Application } from './src/Application'
+import { Application } from './src/Application';
 
-const app = new Application()
+const app = new Application();
 
 const run = async () => {
-	await app.init()
-}
+  await app.init();
+};
 
-run().then(() => app.listen(3000, () =>
-	console.log(`your server is running on port 3000`)
-))
+run().then(() =>
+  app.listen(3000, () =>
+    console.log(`Server is accessible on http://localhost:3000`)
+  )
+);
